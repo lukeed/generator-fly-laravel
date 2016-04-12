f205e3d9e44739959d33e921dc1defaac2fd9ea8
luke <luke.edwards05@gmail.com>
2016-04-12 08:59:50 -0700

    replace eslint with fly-xo. upgrade to fly ^1.2

--- generators/app/index.js
+++ generators/app/index.js
@@ -190,10 +190,10 @@ module.exports = yeoman.generators.Base.extend({
   },
 
   install: function() {
-    var plugins = ['eslint', 'fly-ava']; // @BUG: Missing Deps without it????
+    var plugins = [];
     if (this.testrunner) plugins.push('fly-'+this.testrunner);
     if (this.props.useProcessor) plugins.push('fly-'+this.cssTool);
-    if (this.props.useXO) plugins.push('eslint-config-xo');
+    if (this.props.useXO) plugins.push('fly-xo');
 
     this.spawnCommand('composer', ['install', '--quiet']);
 
